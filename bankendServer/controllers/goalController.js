191b012a5334bc6fd597f6874ba83135116ee3ef
wolfiee42 <saifalislam2022@gmail.com>
2024-02-02 21:52:45 +0600

    adding text as a body from postman. after that adding errormiddleware for express built-in error. and lastly, downloading npm package called express-async-handler from async await purpose because boiii we are about to jump to learn mongoose.

--- bankendServer/controllers/goalController.js
+++ bankendServer/controllers/goalController.js
@@ -1,33 +1,39 @@
+const asynchandler = require('express-async-handler')
+
 // @desc    get Goals
 // @route   GET /api/goals
 // @access  PRIVATE
-const getGoals = (req, res) => {
+const getGoals = asynchandler(async (req, res) => {
     res.json({ message: "get goal" })
-};
+});
 
 
 // @desc    set Goals
 // @route   POST /api/goal/:id
 // @access  PRIVATE
-const setGoal = (req, res) => {
+const setGoal = asynchandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error("Please add a text field")
+    };
     res.json({ message: "set goal" })
-};
+});
 
 
 // @desc    update Goals
 // @route   PUT /api/goal/:id
 // @access  PRIVATE
-const updateGoal = (req, res) => {
+const updateGoal = asynchandler(async (req, res) => {
     res.json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 
 // @desc    delete Goals
 // @route   DELETE /api/goals
 // @access  PRIVATE
-const deleteGoal = (req, res) => {
+const deleteGoal = asynchandler(async (req, res) => {
     res.json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 
 module.exports = {
--- bankendServer/server.js
+++ bankendServer/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routers/goalRoutes'))
+app.use('/api/goals', require('./routers/goalRoutes'));
 
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
-})
+});
