7c26d0e76309c3821975747e86299b42e0e45e6b
wolfiee42 <saifalislam2022@gmail.com>
2024-02-02 20:26:50 +0600

    testing

--- bankendServer/routers/goalRoutes.js
+++ bankendServer/routers/goalRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
 
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+// router.route('/').get(getGoals).post(setGoal);
+// router.route('/:id').put(updateGoal).delete(deleteGoal);
 
 
 
-// router.get('/', getGoals);
+router.get('/', getGoals);
 
-// router.post('/', setGoal);
+router.post('/', setGoal);
 
-// router.put('/:id', updateGoal);
+router.put('/:id', updateGoal);
 
-// router.delete('/:id', deleteGoal);
+router.delete('/:id', deleteGoal);
 
 
 module.exports = router;
